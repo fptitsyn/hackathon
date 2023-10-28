7486eaae51e336693364eff9d19fbf6e88b08ce6
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-28 20:11:35 +0300

    Started working on choosing enemy

---
+++ Assets/Scripts/Tab Scripts/TabPanel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabPanel : MonoBehaviour
+{
+    public List<GameObject> buttons;
+    public Enemy currentEnemySelected;
+}
