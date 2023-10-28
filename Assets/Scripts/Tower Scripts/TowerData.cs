7757919daf13ede81cb8889a637a4104245cd942
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-28 15:46:35 +0300

    Updated prefabs

--- Assets/Scripts/Tower Scripts/TowerData.cs
+++ Assets/Scripts/Tower Scripts/TowerData.cs
@@ -16,33 +16,4 @@ public class TowerData : MonoBehaviour
     public List<TowerType> types;
 
     [SerializeField] public TowerType CurrentType;
-    /*
-     private TowerType _currentType;
-
-    public TowerType CurrentType
-    {
-        get => _currentType;
-        set
-        {
-            _currentType = value;
-            int currentTypeIndex = types.IndexOf(_currentType);
-
-            GameObject typeVisual = types[currentTypeIndex].visualization;
-            for (int i = 0; i < types.Count; i++)
-            {
-                if (typeVisual != null)
-                {
-                    if (i == currentTypeIndex)
-                    {
-                        types[i].visualization.SetActive(true);
-                    }
-                    else
-                    {
-                        types[i].visualization.SetActive(false);
-                    }
-                }
-            }
-        }
-    }
-    */
 }
