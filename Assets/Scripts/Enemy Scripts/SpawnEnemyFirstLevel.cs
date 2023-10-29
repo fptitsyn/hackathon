f01b1b4f5b7a71b7dae386d964018c4d6e553311
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-29 08:58:30 +0300

    Added spawn with a mouse click

--- Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
+++ Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,12 +9,25 @@ public class SpawnEnemy : MonoBehaviour{
     [SerializeField] Castle castle;
     [SerializeField] Enemy[] enemies;
 
+    private void OnMouseDown()
+    {
+        Spawn();
+    }
+
     public void Spawn(){
+        /*
         for(int i = 0;i < enemies.Length; i++){
             FollowWaypoints followWaypoint = new FollowWaypoints();
             followWaypoint.enemy = Instantiate(enemies[i], this.transform.position, this.transform.rotation);
             followWaypoint.enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
             followWaypoint.enemy.GetComponent<FollowWaypoints>().castle = castle;
         }
+        */
+        TabPanel tab = FindObjectOfType<TabPanel>();
+        Enemy enemyToSpawn = tab.currentEnemySelected;
+        FollowWaypoints followWaypoint = new FollowWaypoints();
+        followWaypoint.enemy = Instantiate(enemyToSpawn, transform.position, transform.rotation);
+        followWaypoint.enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+        followWaypoint.enemy.GetComponent<FollowWaypoints>().castle = castle;
     }
 }
