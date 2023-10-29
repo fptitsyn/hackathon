974ef337d911b3b6b9693dc33c55e41f47ccb8b8
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-29 11:34:13 +0300

    Reverted back

--- Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
+++ Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
@@ -7,27 +7,26 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour{
     public GameObject[] waypoints;
     [SerializeField] Castle castle;
-    [SerializeField] Enemy[] enemies;
-
+    private List<Enemy> _enemies = new List<Enemy>();
     private void OnMouseDown()
     {
         Spawn();
     }
 
-    public void Spawn(){
-        /*
-        for(int i = 0;i < enemies.Length; i++){
+    private void Spawn(){
+        TabPanel tab = FindObjectOfType<TabPanel>();
+        Enemy enemyToSpawn = tab.currentEnemySelected;
+        _enemies.Add(enemyToSpawn);
+    }
+
+    public void MoveEnemies()
+    {
+        foreach (var enemy in _enemies)
+        {
             FollowWaypoints followWaypoint = new FollowWaypoints();
-            followWaypoint.enemy = Instantiate(enemies[i], this.transform.position, this.transform.rotation);
+            followWaypoint.enemy = Instantiate(enemy, transform.position, transform.rotation);
             followWaypoint.enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
             followWaypoint.enemy.GetComponent<FollowWaypoints>().castle = castle;
         }
-        */
-        TabPanel tab = FindObjectOfType<TabPanel>();
-        Enemy enemyToSpawn = tab.currentEnemySelected;
-        FollowWaypoints followWaypoint = new FollowWaypoints();
-        followWaypoint.enemy = Instantiate(enemyToSpawn, transform.position, transform.rotation);
-        followWaypoint.enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
-        followWaypoint.enemy.GetComponent<FollowWaypoints>().castle = castle;
     }
 }
