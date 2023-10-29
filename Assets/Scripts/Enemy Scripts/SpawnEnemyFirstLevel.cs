135df59d85773031caa4e5cea7ef03832ee87e82
IvanMay <IvanMay44@yandex.ru>
2023-10-29 13:13:22 +0300

    Спавн по кнопкам + пауза
    Совмещение спавна мобов с паузой

--- Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
+++ Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
@@ -6,27 +6,58 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour{
     public GameObject[] waypoints;
-    [SerializeField] Castle castle;
-    private List<Enemy> _enemies = new List<Enemy>();
-    private void OnMouseDown()
-    {
-        Spawn();
-    }
+    [SerializeField] private GameObject settings;
 
-    private void Spawn(){
+    public void Spawn(){
         TabPanel tab = FindObjectOfType<TabPanel>();
-        Enemy enemyToSpawn = tab.currentEnemySelected;
-        _enemies.Add(enemyToSpawn);
-    }
+        Enemy enemy = Instantiate(tab.currentEnemySelected, this.transform.position, this.transform.rotation);
+        enemy.AddComponent<FollowWaypoints>();
+        enemy.AddComponent<EnemyDestructionDelegate>();
+        enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+        enemy.GetComponent<FollowWaypoints>().settings = settings;
+        Enemy[] oldFollowWaypoints = new Enemy[settings.GetComponent<Level1Script>().enemies.Length];
+        for (int i = 0; i < settings.GetComponent<Level1Script>().enemies.Length; i++)
+            oldFollowWaypoints[i] = settings.GetComponent<Level1Script>().enemies[i];
+        settings.GetComponent<Level1Script>().enemies = new Enemy[settings.GetComponent<Level1Script>().enemies.Length + 1];
+        for (int i = 0; i < oldFollowWaypoints.Length; i++)
+            settings.GetComponent<Level1Script>().enemies[i] = oldFollowWaypoints[i];
+        settings.GetComponent<Level1Script>().enemies[oldFollowWaypoints.Length] = enemy;
+   }
 
-    public void MoveEnemies()
-    {
-        foreach (var enemy in _enemies)
-        {
+    private void OnMouseDown() => Spawn();
+
+    /*
+    public void SpawnAllTypes(){
+        settings.GetComponent<Level1Script>().enemies = new Enemy[settings.GetComponent<Level1Script>().enemies.Length + enemies.Length];
+        for (int i = 0; i < enemies.Length; i++){
+            Enemy enemy = Instantiate(enemies[i], this.transform.position, this.transform.rotation);
+            enemy.AddComponent<FollowWaypoints>();
+            enemy.AddComponent<EnemyDestructionDelegate>();
+            enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+            enemy.GetComponent<FollowWaypoints>().settings = settings;
+            settings.GetComponent<Level1Script>().enemies[i] = enemy;
+        }
+        //TabPanel tab = FindObjectOfType<TabPanel>();
+        //Enemy enemyToSpawn = tab.currentEnemySelected;
+        //_enemies.Add(enemyToSpawn);
+
+        //TabPanel tab = FindObjectOfType<TabPanel>();
+        //Enemy enemy = Instantiate(tab.currentEnemySelected, transform.position, transform.rotation);
+        //enemy.AddComponent<FollowWaypoints>();
+        //enemy.AddComponent<EnemyDestructionDelegate>();
+        //enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+        //enemy.GetComponent<FollowWaypoints>().settings = settings;
+        }
+    */
+    /*
+    public void MoveEnemies(){
+        settings.GetComponent<Level1Script>().enemies = new Enemy[_enemies.Count];
+        foreach (var enemy in _enemies){
             FollowWaypoints followWaypoint = new FollowWaypoints();
             followWaypoint.enemy = Instantiate(enemy, transform.position, transform.rotation);
             followWaypoint.enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
-            followWaypoint.enemy.GetComponent<FollowWaypoints>().castle = castle;
+            followWaypoint.enemy.GetComponent<FollowWaypoints>().settings = settings;
+            settings.GetComponent<Level1Script>().enemies[settings.GetComponent<Level1Script>().enemies.Length - 1] = enemy;
         }
-    }
+    }*/
 }
--- Assets/Scripts/FollowWaypoints.cs
+++ Assets/Scripts/FollowWaypoints.cs
@@ -5,26 +5,21 @@ public class FollowWaypoints : MonoBehaviour
     public GameObject[] waypoints;
     private int _currentWaypoint;
     [SerializeField] private float distance = 0.01f;
-    [SerializeField] public Castle castle;
     [SerializeField] public Enemy enemy;
+    [SerializeField] public GameObject settings;
 
-    void Update()
-    {
+    void Update(){
+        if (!settings.GetComponent<Level1Script>().isPlayGame)
+            return;
         if (Vector2.Distance(transform.position, waypoints[_currentWaypoint].transform.position) < distance)
-        {
             _currentWaypoint++;
-        }
-
-        if (_currentWaypoint >= waypoints.Length)
-        {
-            castle.hp -= enemy.attack;
+        if (_currentWaypoint >= waypoints.Length){
+            settings.GetComponent<Level1Script>().castle.hp -= enemy.attack;
             Destroy(gameObject);
             return;
         }
-
-        transform.LookAt(waypoints[_currentWaypoint].transform);
-        Transform transform1;
-        (transform1 = transform).Translate(0, 0, enemy.speed * Time.deltaTime);
-        transform1.eulerAngles = new Vector3(0, transform1.eulerAngles.y, 0);
+        this.transform.LookAt(waypoints[_currentWaypoint].transform);
+        this.transform.Translate(0, 0, GetComponentInParent<Enemy>().speed * Time.deltaTime);
+        this.transform.eulerAngles = new Vector3(0, 0, this.transform.eulerAngles.z);
     }
 }
