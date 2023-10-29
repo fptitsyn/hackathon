da4013faef5415ef08106921393ac6846d3fc335
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-29 17:04:31 +0300

    а как

--- Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
+++ Assets/Scripts/Enemy Scripts/SpawnEnemyFirstLevel.cs
@@ -8,22 +8,27 @@ public class SpawnEnemy : MonoBehaviour
     [SerializeField] private GameObject settings;
     private List<Enemy> _enemies = new List<Enemy>();
     [SerializeField] Castle castle;
+    public bool isStarted;
     
     private void OnMouseDown() => Spawn();
 
-    public void Spawn(){
+    public void Spawn()
+    {
         TabPanel tab = FindObjectOfType<TabPanel>();
         Enemy enemyToSpawn = tab.currentEnemySelected;
         Enemy enemy = Instantiate(enemyToSpawn, transform.position, transform.rotation);
+        enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+        enemy.GetComponent<FollowWaypoints>().castle = castle;
         _enemies.Add(enemy);
     }
 
     public void MoveEnemies()
     {
-        foreach (var enemy in _enemies)
-        {
-            enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
-            enemy.GetComponent<FollowWaypoints>().castle = castle;
-        }
+        isStarted = true;
+        // foreach (var enemy in _enemies)
+        // {
+        //     enemy.GetComponent<FollowWaypoints>().waypoints = waypoints;
+        //     enemy.GetComponent<FollowWaypoints>().castle = castle;
+        // }
     }
 }
--- Assets/Scripts/FollowWaypoints.cs
+++ Assets/Scripts/FollowWaypoints.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FollowWaypoints : MonoBehaviour
@@ -9,17 +10,28 @@ public class FollowWaypoints : MonoBehaviour
     [SerializeField] public Enemy enemy;
     [SerializeField] public GameObject settings;
 
-    void Update(){
-        if (Vector2.Distance(transform.position, waypoints[_currentWaypoint].transform.position) < distance)
-            _currentWaypoint++;
-        if (_currentWaypoint >= waypoints.Length){
-            castle.hp -= enemy.attack;
-            Destroy(gameObject);
-            return;
+    private SpawnEnemy _spawner;
+    
+    private void Start()
+    {
+        _spawner = FindObjectOfType<SpawnEnemy>();
+    }
+
+    void Update()
+    {
+        if (_spawner.isStarted)
+        {
+            if (Vector2.Distance(transform.position, waypoints[_currentWaypoint].transform.position) < distance)
+                _currentWaypoint++;
+            if (_currentWaypoint >= waypoints.Length){
+                castle.hp -= enemy.attack;
+                Destroy(gameObject);
+                return;
+            }
+            transform.LookAt(waypoints[_currentWaypoint].transform);
+            Transform transform1;
+            (transform1 = transform).Translate(0, 0, enemy.speed * Time.deltaTime);
+            transform1.eulerAngles = new Vector3(0, 0, transform1.eulerAngles.z);
         }
-        transform.LookAt(waypoints[_currentWaypoint].transform);
-        Transform transform1;
-        (transform1 = transform).Translate(0, 0, GetComponentInParent<Enemy>().speed * Time.deltaTime);
-        transform1.eulerAngles = new Vector3(0, 0, transform1.eulerAngles.z);
     }
 }
